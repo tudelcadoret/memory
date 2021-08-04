fa292a4dd5d254a7aae9d61d54737297e55db7f8
tudel <tudel.cadoret@gmail.com>
2021-08-04 18:15:25 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <img src="./assets/logo.png" alt="logo">
+  <img src="./assets/1.jpg" alt="logo">
+  <img :src="src" :alt="name">
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: 'My First Vue App ! =D',
+      src: "./assets/1.jpg",
+      name: "logo"
+    }
   }
 }
 </script>
@@ -22,5 +27,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
